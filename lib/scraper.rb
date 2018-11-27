12a86b1f21ff94c4dd3848dea9fb0177d17d7c07
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-27 16:33:14 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,75 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
+  attr_accessor :student_hashes, :bio, :quote, :twitter, :linkedin, :github, :blog, :student_hashe
+  @student_hashes = []
+  @student_hash = {}
+  
+
   def self.scrape_index_page(index_url)
-    
+     
+    doc = Nokogiri::HTML(open("#{index_url}"))
+    students = doc.css("a")
+    students.each do |card| 
+      a = card.css("h4").text
+      b = card.css("p").text 
+      c = a.gsub(' ', '-').downcase
+      e = "students/#{c}.html"
+      d = {}
+      
+      d[:name] = a 
+      d[:location] = b 
+      d[:profile_url] = e
+      @student_hashes << d 
+    end 
+    @student_hashes.delete_at(0)
+    @student_hashes
   end
 
   def self.scrape_profile_page(profile_url)
+    doc = Nokogiri::HTML(open("#{profile_url}"))
+    if doc.css("p").text 
+      bio = doc.css("p").text 
+    end 
+    if doc.css("div")[6].text
+      quote = doc.css("div")[6].text 
+    end 
+    if doc.css("a")[1]["href"]
+      twitter = doc.css("a")[1]["href"]
+    end 
+    if doc.css("a")[2]["href"]
+      linkedin = doc.css("a")[2]["href"]
+    end 
+    if doc.css("a")[3]["href"]
+      github = doc.css("a")[3]["href"]
+    end 
+    if doc.css("a")[4]["href"]
+      blog = doc.css("a")[4]["href"]
+    end 
     
+    if twitter
+      @student_hash[:twitter] = twitter 
+    end 
+    if linkedin
+      @student_hash[:linkedin] = linkedin
+    end 
+    if github
+      @student_hash[:github] = github
+    end 
+    if blog 
+      @student_hash[:blog] = blog 
+    end 
+    if bio 
+      @student_hash[:bio] = bio 
+    end 
+    if quote 
+      @student_hash[:profile_quote] = quote 
+    end
+    @student_hash
+     
   end
 
 end
--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,30 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name = student_hash[:name] 
+    @location = student_hash[:location]
+    @profile_url = student_hash[:profile_url]
+    @@all << self 
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |hash|
+      x = Student.new(hash)
+    end 
   end
 
   def add_student_attributes(attributes_hash)
-    
+    x = @@all.find {|student| student.name == attributes_hash.name} 
+    x[:bio] = attributes_hash[:bio]
+    x[:profile_quote] = attributes_hash[:profile_quote]
+    x[:twitter] = attributes_hash[:twitter]
+    x[:linkedin] = attributes_hash[:linkedin]
+    x[:github] = attributes_hash[:github]
+    x[:blog] = attributes_hash[:blog]
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
